2c6eaecb9c7c96fcd01c8f5b22d73b2aaf2dfa10
catarinafilipe <catarina.c.filipe@tecnico.ulisboa.pt>
2020-11-15 06:08:24 +0000

    clustering

--- Libs/ds_functions.py
+++ Libs/ds_functions.py
@@ -1,12 +1,18 @@
 import itertools
+import math
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import warnings
 import sklearn.metrics as metrics
 import config as cfg
 import datetime as dt
+import matplotlib.colors as colors
 
+
+COLORS = colors.CSS4_COLORS
 
 mdates._reset_epoch_test_example()
 mdates.set_epoch('0000-12-31T00:00:00')  # old epoch (pre MPL 3.3)
@@ -119,15 +125,11 @@ def plot_confusion_matrix(cnf_matrix: np.ndarray, classes_names: np.ndarray,
         ax.text(j, i, format(cm[i, j], fmt), color='w', horizontalalignment="center")
 
 
-def plot_evaluation_results(labels: np.ndarray, trn_y, prd_trn, tst_y, prd_tst, *args):
+def plot_evaluation_results(labels: np.ndarray, trn_y, prd_trn, tst_y, prd_tst):
     cnf_mtx_trn = metrics.confusion_matrix(trn_y, prd_trn, labels)
     tn_trn, fp_trn, fn_trn, tp_trn = cnf_mtx_trn.ravel()
     cnf_mtx_tst = metrics.confusion_matrix(tst_y, prd_tst, labels)
     tn_tst, fp_tst, fn_tst, tp_tst = cnf_mtx_tst.ravel()
-
-    set_title = "Model's performance over Train and Test sets"
-    if args:
-        set_title = args[0]
 
     evaluation = {'Accuracy': [(tn_trn + tp_trn) / (tn_trn + tp_trn + fp_trn + fn_trn),
                                (tn_tst + tp_tst) / (tn_tst + tp_tst + fp_tst + fn_tst)],
@@ -136,7 +138,7 @@ def plot_evaluation_results(labels: np.ndarray, trn_y, prd_trn, tst_y, prd_tst, 
                   'Precision': [tp_trn / (tp_trn + fp_trn), tp_tst / (tp_tst + fp_tst)]}
 
     fig, axs = plt.subplots(1, 2, figsize=(2 * HEIGHT, HEIGHT))
-    multiple_bar_chart(['Train', 'Test'], evaluation, ax=axs[0], title=title1)
+    multiple_bar_chart(['Train', 'Test'], evaluation, ax=axs[0], title="Model's performance over Train and Test sets")
     plot_confusion_matrix(cnf_mtx_tst, labels, ax=axs[1])
 
 
@@ -153,3 +155,43 @@ def plot_roc_chart(models: dict, tstX: np.ndarray, tstY: np.ndarray, ax: plt.Axe
     for clf in models.keys():
         metrics.plot_roc_curve(models[clf], tstX, tstY, ax=ax, marker='', linewidth=1)
     ax.legend(loc="lower right")
+
+
+def plot_clusters(data, var1st, var2nd, clusters, centers, n_clusters: int, title: str,  ax: plt.Axes = None):
+    if ax is None:
+        ax = plt.gca()
+    ax.scatter(data.iloc[:, var1st], data.iloc[:, var2nd], c=clusters, alpha=0.5)
+    for k, col in zip(range(n_clusters), COLORS):
+        cluster_center = centers[k]
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=6)
+    ax.set_title(title)
+    ax.set_xlabel('var' + str(var1st))
+    ax.set_ylabel('var' + str(var2nd))
+
+
+def compute_centroids(data: pd.DataFrame, labels: np.ndarray) -> list:
+    n_vars = data.shape[1]
+    ext_data = pd.concat([data, pd.DataFrame(labels)], axis=1)
+    ext_data.columns = list(data.columns) + ['cluster']
+    clusters = pd.unique(labels)
+    n_clusters = len(clusters)
+    centers = [0] * n_clusters
+    for k in range(-1, n_clusters):
+        if k != -1:
+            cluster = ext_data[ext_data['cluster'] == k]
+            centers[k] = list(cluster.sum(axis=0))
+            centers[k] = [centers[k][j]/len(cluster) if len(cluster) > 0 else 0 for j in range(n_vars)]
+        else:
+            centers[k] = [0]*n_vars
+
+    return centers
+
+
+def compute_mse(X: np.ndarray, labels: np.ndarray, centroids: np.ndarray) -> float:
+    n = len(X)
+    centroid_per_record = [centroids[labels[i]] for i in range(n)]
+    partial = X - centroid_per_record
+    partial = list(partial * partial)
+    partial = [sum(el) for el in partial]
+    partial = sum(partial)
+    return math.sqrt(partial) / (n-1)
